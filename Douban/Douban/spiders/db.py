5527b34bc3b85a131fc9773ddda02f86079e471e
t7for <85014858+t7for@users.noreply.github.com>
2025-05-12 11:31:07 +0800

    Add files via upload

---
+++ Douban/Douban/spiders/db.py
@@ -0,0 +1,28 @@
+import scrapy
+from Douban.items import DoubanItem
+
+class DbSpider(scrapy.Spider):
+    name = "db"
+    allowed_domains = ["movie.douban.com"]
+    start_urls = ["https://movie.douban.com/top250"]
+
+    def parse(self, response):
+
+        print(response.request.headers['User-Agent'])
+
+        #获取列表
+        movie_list=response.xpath('//*[@class="info"]')
+        
+
+        for movie in movie_list:
+            #实例化DoubanItem
+            item=DoubanItem()
+            #定义爬取信息
+            item['name']=movie.xpath('./div[1]/a/span[1]/text()').extract_first()
+            yield item
+        
+
+        url=response.xpath('//span[@class="next"]/a').extract_first()
+        if url!=None:
+            url=response.urljoin(url)
+            yield scrapy.Request(url=url,callback=self.parse)
